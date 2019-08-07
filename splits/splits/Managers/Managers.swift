411deb5afcb5dd8c95f8091eb33ff293f2cbc00d
Ken Cluff <kencluffutah@gmail.com>
2019-08-07 07:53:37 -0600

    Basic app infrastructure

---
+++ splits/splits/Managers/Managers.swift
@@ -0,0 +1,9 @@
+//
+//  Managers.swift
+//  splits
+//
+//  Created by Kenneth Cluff on 8/7/19.
+//  Copyright © 2019 Kenneth Cluff. All rights reserved.
+//
+
+import Foundation
---
+++ splits/splits/Models/Models.swift
@@ -0,0 +1,9 @@
+//
+//  Models.swift
+//  splits
+//
+//  Created by Kenneth Cluff on 8/7/19.
+//  Copyright © 2019 Kenneth Cluff. All rights reserved.
+//
+
+import Foundation

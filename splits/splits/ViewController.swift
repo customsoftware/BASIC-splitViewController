d6c466ef2c1e26feff0de7475af6880600c2f685
Ken Cluff <kencluffutah@gmail.com>
2019-08-07 06:56:11 -0600

    Initial

---
+++ splits/splits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  splits
+//
+//  Created by Kenneth Cluff on 8/7/19.
+//  Copyright © 2019 Kenneth Cluff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

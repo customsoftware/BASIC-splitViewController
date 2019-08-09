a83fb58dc68998fab0bfccf65c16d8a630efc4b6
CustomSoftware <kcluff50@gmail.com>
2019-08-09 11:35:40 -0600

    kind working in compact mode

--- splits/splits/Controllers/DetailViewController.swift
+++ splits/splits/Controllers/DetailViewController.swift
@@ -17,31 +17,34 @@ class DetailViewController: UIViewController, ProgramBuildable {
     
     var controllingEnum: TestDetails? {
         didSet {
-            guard let controllingEnum = controllingEnum else { return }
+            refreshView()
+        }
+    }
+    
+    func refreshView() {
+        guard let controllingEnum = controllingEnum else { return }
+        
+        if let navC = navigationController,
+            navC.viewControllers.count > 1 {
             
-            if let navC = navigationController,
-                navC.viewControllers.count > 1 {
-                
-                navC.popViewController(animated: false)
-                
-            }
+            navC.popViewController(animated: false)
+        }
+        
+        switch controllingEnum {
+        case .exampleOne:
+            visibleViewController = TestVCOne()
             
-            switch controllingEnum {
-            case .exampleOne:
-                visibleViewController = TestVCOne()
-                
-            case .exampleTwo:
-                visibleViewController = TestVCTwo()
-                
-            case .exampleThree:
-                visibleViewController = TestVCThree()
-                
-            }
+        case .exampleTwo:
+            visibleViewController = TestVCTwo()
             
-            if let navC = navigationController,
-                let vc = visibleViewController {
-                navC.pushViewController(vc, animated: false)
-            }
+        case .exampleThree:
+            visibleViewController = TestVCThree()
+            
+        }
+        
+        if let navC = navigationController,
+            let vc = visibleViewController {
+            navC.pushViewController(vc, animated: false)
         }
     }
     
@@ -55,22 +58,5 @@ class DetailViewController: UIViewController, ProgramBuildable {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-    }
-    
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        if traitCollection.horizontalSizeClass == .compact {
-            print("We're compact")
-            // Show master view
-            navigationController?.dismiss(animated: true, completion: nil)
-        } else {
-            print("were normal")
-            splitViewController?.preferredDisplayMode = .automatic
-        }
     }
 }
--- splits/splits/Controllers/RootTableViewController.swift
+++ splits/splits/Controllers/RootTableViewController.swift
@@ -33,25 +33,47 @@ class RootTableViewController: UITableViewController, ProgramBuildable {
         tableView.register(DemoCell.self, forCellReuseIdentifier: cellID)
     }
     
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let segueID = segue.identifier,
+//            let theEnum = sender as? TestDetails,
+//            let option = TestSegues(rawValue: segueID) else { return }
+//        switch option {
+//        case .testSegueID:
+//            let newVC = segue.destination as! DetailViewController
+//            newVC.controllingEnum = theEnum
+//        }
+//    }
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return TestDetails.allCases.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! DemoCell
-
         cell.controllingEnum = TestDetails.allCases[indexPath.row]
-        
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let passedEnum = TestDetails.allCases[indexPath.row]
-        guard let detailNav = splitViewController?.viewControllers.last as? UINavigationController,
-            let detailView = detailNav.viewControllers.first as? DetailViewController else { return }
-        detailView.controllingEnum = passedEnum
+        showEnum(passedEnum)
+    }
+    
+    private func showEnum(_ theEnum: TestDetails) {
+        if traitCollection.horizontalSizeClass == .compact {
+            print("We're compact")
+            let newVC = DetailViewController()
+            newVC.controllingEnum = theEnum
+            let newNavC = UINavigationController(rootViewController: newVC)
+            splitViewController?.showDetailViewController(newNavC, sender: nil)
+            newVC.refreshView()
+        } else {
+            print("were normal")
+            guard let detailNav = splitViewController?.viewControllers.last as? UINavigationController,
+                let detailView = detailNav.viewControllers.first as? DetailViewController else { return }
+            detailView.controllingEnum = theEnum
+        }
     }
 }
 

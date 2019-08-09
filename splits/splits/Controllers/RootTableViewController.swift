2bdc305ea792745889805f3cbde4e072f431f9a3
Ken Cluff <kencluffutah@gmail.com>
2019-08-09 08:04:33 -0600

    Dummy data in master view

--- splits/splits/Controllers/RootTableViewController.swift
+++ splits/splits/Controllers/RootTableViewController.swift
@@ -8,77 +8,39 @@
 
 import UIKit
 
+enum TestDetails: CaseIterable {
+    case exampleOne
+    case exampleTwo
+    case exampleThree
+}
+
+
 class RootTableViewController: UITableViewController, ProgramBuildable {
+    let cellID = "DemoCellID"
+    
     func createControls() {
         
     }
     
     override func loadView() {
         super.loadView()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        navigationItem.title = "Test Master"
+        tableView.register(DemoCell.self, forCellReuseIdentifier: cellID)
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return TestDetails.allCases.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! DemoCell
 
-        // Configure the cell...
-
+        cell.controllingEnum = TestDetails.allCases[indexPath.row]
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -90,4 +52,26 @@ class RootTableViewController: UITableViewController, ProgramBuildable {
     }
     */
 
+}
+
+class DemoCell: UITableViewCell {
+    var controllingEnum: TestDetails? {
+        didSet{
+            guard let controllingEnum = controllingEnum else { return }
+            switch controllingEnum {
+            case .exampleOne:
+                textLabel?.text = "The First Choice"
+                contentView.backgroundColor = UIColor.lightGray
+                
+            case .exampleTwo:
+                textLabel?.text = "The Second Choice"
+                contentView.backgroundColor = UIColor.gray
+                textLabel?.textColor = .white
+                
+            case .exampleThree:
+                textLabel?.text = "The Third Choice"
+                textLabel?.textColor = .darkGray
+            }
+        }
+    }
 }
